d27355a5b11c90f2beee5960492be7c8bed931ae
Arcillas <l21140773@queretaro.tecnm.mx>
2023-09-01 10:04:36 -0600

    Xd, copy and paste the objet L xd

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LYA1_Lexico
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            try
+            {
+                using (Lexico L = new Lexico())
+                {
+                    L.setContenido("");
+                    L.setClasificacion(Token.Tipos.Identificador);
+                    
+                    L.nextToken();
+                    L.nextToken();
+                    L.nextToken();
+                    L.nextToken();
+                    L.nextToken();
+                    L.nextToken();
+                    L.nextToken();
+                    L.nextToken();
+                    L.nextToken();
+
+                    Console.WriteLine(L.getContenido());
+                    Console.WriteLine(L.getClasificacion());
+
+                }
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: "+e.Message);
+            }
+        }
+    }
+}
