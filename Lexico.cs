0b027827a1bcc077b2e3fca24f6f94c60a4625c4
Arcillas <l21140773@queretaro.tecnm.mx>
2023-09-05 15:22:46 -0600

    Add the nextToken method, is primitive filter of words

--- Lexico.cs
+++ Lexico.cs
@@ -45,16 +45,107 @@ namespace LYA1_Lexico
             }
             else if (c == '='){
                 setClasificacion(Tipos.Asignacion);
+                if ((c = (char)archivo.Peek()) == '='){
+                    setClasificacion(Tipos.OperadorRelacional);
+                    buffer += c;
+                    archivo.Read();
+                }
 
             }
+            else if(c == '+'){
+                setClasificacion(Tipos.OperadorTermino);
+
+                if((c = (char)archivo.Peek()) == '+'){
+                    setClasificacion(Tipos.IncrementoTermino);
+                    buffer += c;
+                    archivo.Read();
+                }
+                if((c = (char)archivo.Peek()) == '='){
+                    setClasificacion(Tipos.IncrementoTermino);
+                    buffer += c;
+                    archivo.Read();
+                }
+                
+            }
+            else if(c == '-'){
+                setClasificacion(Tipos.OperadorTermino);
+
+                if((c = (char)archivo.Peek()) == '-' ){
+                    setClasificacion(Tipos.IncrementoTermino);
+                    buffer += c;
+                    archivo.Read();
+                }
+                if((c = (char)archivo.Peek()) == '='){
+                    setClasificacion(Tipos.IncrementoTermino);
+                    buffer += c;
+                    archivo.Read();
+                }
+                
+            }
+
+            else if (c == '*' || c == '/' || c == '%'){
+                setClasificacion(Tipos.OperadorFactor);
+                if((c = (char)archivo.Peek()) == '='){
+                    setClasificacion(Tipos.OperadorFactor);
+                    buffer += c;
+                    archivo.Read();
+                }
+            }
+
             else if (c == ';'){
                 setClasificacion(Tipos.FinSentencia);
             }
+            else if (c == '{'){
+                setClasificacion(Tipos.Inicio);
+            }
+            else if (c == '}'){
+                setClasificacion(Tipos.Fin);
+            }
+
+            else if (c == '!'){
+                setClasificacion(Tipos.OperadorLogico);
+            }
+
+            else if (c == '&'){
+                if((c = (char)archivo.Peek()) == '&'){
+                    setClasificacion(Tipos.OperadorLogico);
+                    buffer += c;
+                    archivo.Read();
+                }
+                else{
+                    setClasificacion(Tipos.Caracter);
+                }
+            }
+
+            else if (c == '|'){
+                if((c = (char)archivo.Peek()) == '|'){
+                    setClasificacion(Tipos.OperadorLogico);
+                    buffer += c;
+                    archivo.Read();
+                }
+                else{
+                    setClasificacion(Tipos.Caracter);
+                }
+            }
+
+            else if (c == '<' || c == '>' || c == '!'){
+                setClasificacion(Tipos.OperadorRelacional);
+                if((c = (char)archivo.Peek()) == '='){
+                    setClasificacion(Tipos.OperadorRelacional);
+                    buffer += c;
+                    archivo.Read();
+                }
+            }
+
             else{
                 setClasificacion(Tipos.Caracter);
             }
             setContenido(buffer);
-            Log.WriteLine(getContenido() + " : " + getClasificacion());
+            Log.WriteLine("["+ getContenido() + "] : " + getClasificacion());
+        }
+
+        public bool FinArchivo(){
+            return archivo.EndOfStream;
         }
 
     }
